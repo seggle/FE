43d764e12345298f6e245f8f2b092c2c4e21ac2f
minkyung00 <mksin00@naver.com>
2022-01-03 21:41:07 +0900

    Feat: Add Board.vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import './assets/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+var app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
---
+++ src/views/Board.vue
@@ -0,0 +1,71 @@
+<template>
+  <form class="board" v-on:submit.prevent="submitForm">
+    <h1 class="board-header">글쓰기</h1>
+    <div class="title">
+        <label for="board-title">제목</label>
+        <input id="board-title" type="text" placeholder="제목을 입력하세요">
+    </div>
+    <div class="content">
+        <label for="board-content">내용</label>
+        <textarea name="" id="board-content" cols="40" rows="10" placeholder="내용을 입력하세요"></textarea>
+    </div>
+    <button class="board-btn" type="submit" v-on:click="submitForm">작성</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Board'
+}
+</script>
+
+<style>
+.board {
+    padding: 8vh 40vh;
+}
+
+.board-header {
+    font-weight: 800;
+    font-size: 40px;
+    margin-bottom: 5vh;
+}
+
+label {
+    display: block;
+    margin-bottom: 1vh;
+    font-size: 22px;
+}
+
+.title,
+.content {
+    padding: 2vh 2vh;
+}
+
+#board-title,
+#board-content {
+    padding: 1vh 1vh;
+    font-size: 22px;
+    width: 100%;
+    border: 2px solid rgb(221, 221, 221);
+    border-radius: 1vh;
+}
+
+#board-content {
+    resize: none;
+}
+
+#board-title input::placeholder {
+    color:rgb(0, 0, 0, 0.3);
+}
+
+.board-btn {
+    float: right;
+    padding: 1vh 3vh;
+    background-color: var(--navy);
+    color: white;
+    border: none;
+    border-radius: 3vh;
+    font-size: 20px;
+    cursor: pointer;
+}
+</style>
