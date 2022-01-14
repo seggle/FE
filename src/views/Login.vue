e581c7f96c03b590e4d0e7bbebdcc3e13e7e0799
minkyung00 <mksin00@naver.com>
2022-01-14 13:30:11 +0900

    Feat: 로그인 폼 유효성 검사 추가

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,20 @@
 <template>
 <div class="container">
   <h1 id="title">Seggle</h1>
-  <form id="login-form" class="row" @submit.prevent="submitForm">
+  <form id="login-form" class="row" :class="{ 'was-validated': validated }" @submit.prevent="handleLogin" novalidate>
     <div class="id-form">
-      <input v-model="userID" type="text" class="form-control" id="validationID" placeholder="아이디" required>
+      <input v-model="userID" type="text" class="form-control" placeholder="아이디" required>
+      <div class="invalid-feedback">아이디를 입력하세요.</div>
     </div>
     <div class="password-form">
-      <input v-model="userPWD" type="password" class="form-control" id="validationPassword" placeholder="비밀번호" required>
+      <input v-model="password" type="password" class="form-control" placeholder="비밀번호" required>
+      <div class="invalid-feedback">비밀번호를 입력하세요.</div>
     </div>
     <button class="btn" type="submit">로그인</button>
   </form>
   <div class="info-link">
     <a href="/register" class="link-join">회원가입</a>
-    <a href="#" class="link-password">비밀번호 찾기</a>
+    <a href="/find-password" class="link-password">비밀번호 찾기</a>
   </div>
 </div>
 </template>
@@ -25,7 +27,8 @@ export default {
   data () {
     return {
       userID: '',
-      userPWD: ''
+      password: '',
+      validated: false
     }
   },
   methods: {
@@ -33,18 +36,23 @@ export default {
       try {
         const data = {
           user_id: this.userID,
-          user_password: this.userPWD
+          user_password: this.password
         }
         const res = await api.loginUser(data)
-        // state 값 갱신
         this.$store.commit('setToken', res.data.token)
         this.$store.commit('setUserid', res.data.user.user_id)
-        // 브라우저 저장소에 쿠키 저장
         saveAuthToCookie(res.data.token)
         saveUserToCookie(res.data.user.user_id)
         this.$router.push('/')
       } catch (err) {
         console.log(err)
+      }
+    },
+    handleLogin () {
+      if (this.userID !== '' && this.password !== '') {
+        this.submitForm()
+      } else {
+        this.validated = true
       }
     }
   }
@@ -70,9 +78,11 @@ export default {
       border-bottom-left-radius: 0;
     }
     input[type="password"] {
-      margin-bottom: 3rem;
       border-top-left-radius: 0;
       border-top-right-radius: 0;
+    }
+    .password-form {
+      margin-bottom: 3rem;
     }
     .btn {
       padding: 0.5rem 1rem;
